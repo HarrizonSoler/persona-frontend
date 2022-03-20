47e2bf296ce86044f9938ba4eb98f86bdc65291a
Harrizon Soler <harrizon.soler@usantoto.edu.co>
2022-03-20 18:32:49 -0500

    created entities services and main template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadosService } from './services/estados/estados.service';
+import { PaisesService } from './services/paises/paises.service';
+import { PersonaService } from './services/persona/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'personafront';
+export class AppComponent implements OnInit {
+  personaForm!: FormGroup;
+  paises: any;
+  estados: any;
+
+  constructor(
+    public fb: FormBuilder,
+    public estadosService: EstadosService,
+    public paisesService: PaisesService,
+    public personaService: PersonaService
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.personaForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      edad: ['', Validators.required],
+      pais: ['', Validators.required],
+      estado: ['', Validators.required],
+    });
+
+    this.paisesService.getAllPaises().subscribe({
+      next: (v) => this.paises = v,
+      error: (e) => console.error(e)
+    })
+  }
+
+  guardar(): void {
+  }
+
+  cargarEstadosPorPaisId(event: any) {
+    this.estadosService.getAllEstadosByPais(event.target.value).subscribe({
+      next: (v) => this.estados = v,
+      error: (e) => console.error(e)
+    });
+  }
 }
